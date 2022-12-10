abd359cf875fbe80f606988133728bb52ca72234
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2022-12-10 16:31:40 +0100

    createProfilePostHtml function created

--- src/scss/js/api/posts/profile.mjs
+++ src/scss/js/api/posts/profile.mjs
@@ -26,14 +26,35 @@ export async function getProfilePosts() {
     console.log(response);
     const posts = await response.json();
 
-    // createPostsHTML(posts);
+    // createProfilePostHtml(posts);
 
     console.log(posts);
   } catch (error) {
     console.log(error);
   }
 }
-
 getProfilePosts();
 
 //// GET /api/v1/social/profiles/<name>
+
+//****************************GET posts and create html page *********************/
+
+const postCardsContainer = document.getElementById("postCardsContainer");
+
+function createProfilePostHtml(posts) {
+  posts.forEach(function (post) {
+    postCardsContainer.innerHTML += `
+    <div class="d-flex justify-content-center border border-primary my-4 ">                                  
+                                            <div class="card-body">
+                                              <h2 class="card-title">Hi ${post.name}</h2>
+                                              <p class="card-text">${post.email}</p>
+                                              <a href="../../../../../profile/edit/index.html?id=${post.id}" class="btn btn-primary">Edit Post</a>                                      
+                                            </div>
+
+ </div>`;
+  });
+}
+
+// <li>By ${post_count.posts}</li>
+
+//  <img src="${post.media}" class="card-img-top img-thumbnail style="width: 18rem; height : 18rem;" alt="${post.title}"/ >
