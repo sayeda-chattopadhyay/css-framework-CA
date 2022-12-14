480ae13533a14cb9d84491fdb44a377fbb92444f
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2022-12-14 23:14:19 +0100

    moved js file from src folder

--- src/scss/js/handlers/logOut.mjs
+++
@@ -1,9 +0,0 @@
-export function profileLogOut() {
-  const logOutLink = document.getElementById("logOut");
-  console.log(logOutLink);
-
-  logOutLink.addEventListener("click", (event) => {
-    console.log("object");
-    localStorage.removeItem(key);
-  });
-}
