8314de61cd594581522b6333a0f4e11e200de1a4
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2022-12-16 19:18:21 +0100

    changed profileName according to the teacher

--- dist/js/api/profile/post/index.mjs
+++ dist/js/api/profile/post/index.mjs
@@ -7,31 +7,27 @@ const token = load("token");
 
 const profile = load("profile");
 
-const profileName = profile.name;
+const profileName = profile?.name;
 
 const getProfileUrl = API_SOCIAL_URL + action + profileName;
 
 const getProfilePostsUrl = getProfileUrl + "/posts";
 
-
-
 export async function getProfilePosts() {
+  const response = await fetch(getProfilePostsUrl, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
-    const response = await fetch(getProfilePostsUrl, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
+  if (response.ok) {
+    return await response.json();
+  }
 
-    if (response.ok) {
-      return await response.json();
-    }
-  
-    throw new Error(" Fetching posts failed");
+  throw new Error(" Fetching posts failed");
 
-    // createProfilePostHtml(posts);
-  
+  // createProfilePostHtml(posts);
 }
 // getProfilePosts();
 
@@ -42,16 +38,16 @@ export async function getProfilePosts() {
 
 //   posts.forEach(function (post) {
 //     postCardsContainer.innerHTML += ` <div class="d-flex justify-content-center border border-primary my-4" >
-//                                         <div> 
+//                                         <div>
 //                                             <img src="${post.media}" class="card-img-top img-thumbnail style="width: 18rem; height : 18rem;" alt="${post.title}"/ >
 //                                         </div>
 //                                         <div class="card-body">
 //                                           <h2 class="card-title">${post.title}</h2>
-//                                           <p class="card-text">${post.body}</p> 
-//                                           <p class="card-text">Date:${post.created}</p> 
+//                                           <p class="card-text">${post.body}</p>
+//                                           <p class="card-text">Date:${post.created}</p>
 //                                           <a href="../../../../../post/edit/index.html?id=${post.id}" class="btn btn-primary">Edit Post</a>
 //                                           <a href="../../../../../profile/edit/index.html?id=${post.id}" id=${post.id} class="btn btn-primary">Delete Post</a>
-//                                           <a href="../../../../../post/specificPost/index.html?id=${post.id}" class= "btn btn-primary">View Post</a>                                        
+//                                           <a href="../../../../../post/specificPost/index.html?id=${post.id}" class= "btn btn-primary">View Post</a>
 //                                         </div>
 
 //  </div>`;
