ab718306c3e27af3d43ea8410418ee08d5bd26ad
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2022-12-17 23:43:15 +0100

    destructuring code

--- dist/js/templates/posts.mjs
+++ dist/js/templates/posts.mjs
@@ -1,3 +1,5 @@
+import * as listeners from "../listeners/index.mjs";
+
 export function createPostsHTML(posts) {
   const postCardsLists = document.getElementById("postCardsLists");
 
@@ -31,7 +33,11 @@ export function createPostsHTML(posts) {
                                         <div id="tag-container" class="text-muted fst-italic">Tag: ${[
                                           ...post.tags,
                                         ]}</div>
-                                        <a href="../../../post/specificPost/index.html?id=${ post.id}&name=${ post.author.name}" class="btn btn-primary">View Post</a>
+                                        <a href="../../../post/specificPost/index.html?id=${
+                                          post.id
+                                        }&name=${
+      post.author.name
+    }" class="btn btn-primary">View Post</a>
                                       </div>
                                   </div>
                                 </a>
@@ -43,18 +49,23 @@ export function createPostsHTML(posts) {
 
 export function createProfilePostHtml(posts) {
   const postCardsContainer = document.getElementById("postCardsContainer");
+
+  postCardsContainer.innerHTML = "";
+
   posts.forEach(function (post) {
+    const { media, title, body, created, id } = post;
+
     postCardsContainer.innerHTML += ` <div class="d-flex justify-content-center border border-primary my-4" >
                                           <div>
-                                              <img src="${post.media}" class="card-img-top img-thumbnail style="width: 18rem; height : 18rem;" alt="${post.title}"/ >
+                                              <img src="${media}" class="card-img-top img-thumbnail style="width: 18rem; height : 18rem;" alt="${title}"/ >
                                           </div>
                                           <div class="card-body">
-                                            <h2 class="card-title">${post.title}</h2>
-                                            <p class="card-text">${post.body}</p>
-                                            <p class="card-text">Date:${post.created}</p>
-                                            <a href="../../../../../post/edit/index.html?id=${post.id}" class="btn btn-primary">Edit Post</a> 
-                                            <a href="../../../../../post/specificPost/index.html?id=${post.id}" class= "btn btn-primary">View Post</a>
-                                            <a href="" id=${post.id} class="delete-post">Delete Post</a>
+                                            <h2 class="card-title">${title}</h2>
+                                            <p class="card-text">${body}</p>
+                                            <p class="card-text">Date:${created}</p>
+                                            <a href="/post/edit/index.html?id=${id}" class="btn btn-primary">Edit Post</a> 
+                                            <a href="/post/specificPost/index.html?id=${id}" class= "btn btn-primary">View Post</a>
+                                            <button data-delete-id=${id} class="delete-post">Delete Post</button>
                                           </div>
 
    </div>`;
@@ -68,81 +79,14 @@ export function createProfilePostHtml(posts) {
 
 //   specificPostContainer.innerHTML += ` <div class="card" style="width: 18rem;">
 //                                             <div class="">
-//                                                 <img src="${details.media}" class="card-img-top img-thumbnail style="width: 18rem;height : 18rem;" alt="${details.title}"/> 
+//                                                 <img src="${details.media}" class="card-img-top img-thumbnail style="width: 18rem;height : 18rem;" alt="${details.title}"/>
 //                                             </div>
 //                                             <div class="card-body">
 //                                                 <h2 class="card-title">${details.title}</h2>
 //                                                 <p class="card-text">${details.body}</p>
 //                                                 <p class="card-text">By ${details.author}</p>
-//                                                 <p class="card-text">By ${details.created}</p> 
-                                                 
-//                                             </div>    
+//                                                 <p class="card-text">By ${details.created}</p>
+
+//                                             </div>
 //                                         </div>`;
-// }
-
-
-
-
-
-//*****@@@@@@@@@@@@@@@ */
-
-
-
-
-
-//createElement
-
-// const postCardsContainer = document.getElementById("postCardsContainer");
-
-// export function createProfilePostHtml(posts) {
-//   posts.forEach(function (post) {
-//     const card = document.createElement("div");
-//     const cardImage = document.createElement("div");
-//     const img = document.createElement("img");
-//     const cardBody = document.createElement("div");
-//     const cardTitle = document.createElement("h2");
-//     // const cardtext = document.createElement("p");
-//     const cardDate = document.createElement("p");
-//     const viewPost = document.createElement("a");
-//     const editPost = document.createElement("a");
-//     const button = document.createElement("button");
-
-//     postCardsContainer.appendChild(card);
-
-//     card.appendChild(cardImage);
-//     card.appendChild(cardBody);
-//     cardImage.appendChild(img);
-//     cardBody.appendChild(cardTitle);
-//     // cardBody.appendChild(cardText);
-
-//     card.classList.add("d-flex", "my-4");
-
-//     img.classList.add("card-img-top img-thumbnail");
-
-//     cardImage.setAttribute = ("src", post.media);
-
-//     cardBody.classList.add("card-body");
-
-//     cardTitle.classList.add("card-title");
-//     cardTitle.innerHTML = post.title;
-
-//     // cardtext.classList.add("card-text");
-
-//     // cardtext.innerText = post.body;
-
-//     cardDate.classList.add("card-text");
-
-//     cardDate.innerText = post.created;
-
-//     viewPost.classList.add("btn btn-primary");
-
-//     viewPost.innerText = "View Post";
-//     viewPost.href = "../../../../../post/specificPost/index.html?id=${post.id}";
-
-//     editPost.classList.add("btn btn-primary");
-
-//     editPost.href = "../../../../../post/edit/index.html?id=${post.id}";
-
-//     button.classList.add("btn btn-primary");
-//   });
 // }
