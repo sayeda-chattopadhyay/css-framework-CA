291965310a0932429df73405c85fe39cc03cc5b2
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2022-12-17 23:41:25 +0100

    set delete button listener

--- dist/js/views/posts/display.mjs
+++ dist/js/views/posts/display.mjs
@@ -1,36 +1,6 @@
-// import { getPosts } from "../../api/posts/read.mjs";
-
-// export function createPostsHTML(posts) {
-//   const postCardsContainer = document.getElementById("postCardsContainer");
-//   posts.forEach(function (post) {
-//     console.log(post);
-//     postCardsContainer.innerHTML += `
-//       <div class="d-flex justify-content-center border border-primary my-4 ">
-//       <a href="../../../../../post/specificPost/index.html?id=${post.id}" class="card" style="width: 18rem;">
-//                                               <img src="${post.media}" class="card-img-top img-thumbnail style="width: 18rem; height : 18rem;" alt="${post.title}"/ >
-//                                               <div class="card-body">
-//                                                 <h2 class="card-title">${post.title}</h2>
-//                                                 <p class="card-text">${post.body}</p>
-//                                                 <p>By ${post.author.name}</p>
-//                                                 <p>Date: ${post.created}</p>
-//                                                 <p>comment:${post.comments}</p>
-//                                                 <p>reactions:${post.reaction}</p>
-//                                                 <a href="../../../../../post/specificPost/index.html?id=${post.id}" class="btn btn-primary">View Post</a>
-//                                               </div>
-//    </a>
-//    </div>`;
-//   });
-// }
-
-// createPostsHTML(posts);
-
-// getPosts();
-
-//******************************* */
-
 import * as api from "../../api/posts/index.mjs";
 import * as templates from "../../templates/index.mjs";
-//import * as ui from "../../ui/index.mjs";
+import * as listeners from "../../listeners/index.mjs";
 
 export async function displayAllPosts() {
   try {
@@ -53,15 +23,12 @@ export async function displaySearchedPosts(tag) {
   }
 }
 
-////*********** */profile posts
-
-import * as profilePost from "../../api/profile/post/index.mjs";
-
 export async function displayAllprofilePosts() {
   try {
     templates.displayLoading("postCardsContainer"); // ui.displayLoading("postCardsLists");
-    const posts = await profilePost.getProfilePosts();
+    const posts = await api.getProfilePosts();
     templates.createProfilePostHtml(posts);
+    listeners.setDeleteButtonListener();
   } catch (error) {
     templates.displayError("postCardsContainer", error); // ui.displayError("postCardsLists", error)
   }
