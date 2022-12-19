8ab61f8522061ca7c27d1cc876b273583d851f3d
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2022-12-19 22:47:38 +0100

    async func

--- dist/js/listeners/register.mjs
+++ dist/js/listeners/register.mjs
@@ -8,16 +8,20 @@ export function setRegisterFormListener() {
   const form = document.querySelector("#registerForm");
 
   if (form) {
-    form.addEventListener("submit", (event) => {
+    form.addEventListener("submit", async (event) => {
       event.preventDefault();
       const form = event.target;
-      console.log(form);
+      // console.log(form);
       const formData = new FormData(form);
       const profile = Object.fromEntries(formData.entries());
       console.log("showing profile:", profile);
 
       // when we collected the profile data we send it to the Api but we will create the function register(profile)in separate file.( for example- inside apifolder inside auth folder - register.mjs file) and call it here.
-      register(profile);
+      try {
+        await register(profile);
+      } catch (error) {
+        console.log("error:", error);
+      }
     });
   }
 }
