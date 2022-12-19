96aa136179f7843495d384e0fea53e4b35307a72
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2022-12-19 09:04:08 +0100

    code rearranged

--- dist/js/templates/posts.mjs
+++ dist/js/templates/posts.mjs
@@ -10,38 +10,20 @@ export function createPostsHTML(posts) {
   postCardsLists.innerHTML = "";
 
   posts.forEach(function (post) {
-    postCardsLists.innerHTML += `
-                              <div class="card">
-                                <a href="../../../post/specificPost/index.html?id=${
-                                  post.id
-                                }" class="card" style="width: 18rem;">
-                                  <div class="d-flex justify-content-between">
-                                      <div style="overflow:hidden; width:300px; height:300px;"> 
-                                        <img src="${post.media}" alt="${
-      post.title
-    }" class="card-img-top img-thumbnail style="width: 18rem; height : 18rem;"/ >
-                                      </div>
-                                      <div class="card-body">
-                                        <h2 class="card-title">${
-                                          post.title
-                                        }</h2>
-                                        <p class="card-text">${post.body}</p>
-                                        <p>By ${post.author.name}</p>
-                                        <p>Date: ${post.created}</p>
-                                        <p>comment:${post.comments}</p>
-                                        <p>reactions:${post.reaction}</p>
-                                        <div id="tag-container" class="text-muted fst-italic">Tag: ${[
-                                          ...post.tags,
-                                        ]}</div>
-                                        <a href="../../../post/specificPost/index.html?id=${
-                                          post.id
-                                        }&name=${
-      post.author.name
-    }" class="btn btn-primary">View Post</a>
-                                      </div>
-                                  </div>
-                                </a>
-                              </div>`;
+    postCardsLists.innerHTML += ` <a href="/post/specificPost/index.html?id=${post.id}">
+    <div class="d-flex justify-content-between border border-primary my-4 g-3" >
+    <div style="overflow:hidden; width:300px; height:300px;">
+      <img src="${post.media}" alt="${post.title}" width="100%" class="h-100 w-100 img-thumbnail" >
+    </div>  
+    <div class="card-body">
+      <h2 class="card-title">${post.title}</h2>
+      <p class="card-text">${post.body}</p>
+      <p>Date: ${post.created}</p>
+      <p>reactions:${post.reaction}</p>
+      <a href="/post/specificPost/index.html?id=${post.id}&name=${post.author.name}" class="btn btn-primary">View Post</a>
+    </div>
+</div>
+</a>`;
   });
 }
 
@@ -53,17 +35,17 @@ export function createProfilePostHtml(posts) {
   postCardsContainer.innerHTML = "";
 
   posts.forEach(function (post) {
-    const { media, title, body, created, id } = post;
+    const { media, title, body, created, id } = post; // Destructuring
 
-    postCardsContainer.innerHTML += ` <div class="d-flex justify-content-center border border-primary my-4" >
-                                          <div>
-                                              <img src="${media}" class="card-img-top img-thumbnail style="width: 18rem; height : 18rem;" alt="${title}"/ >
+    postCardsContainer.innerHTML += ` <div class=" d-flex justify-content-center border border-primary my-4 g-3" >
+                                          <div class="col-4">
+                                              <img src="${media}" width="100%" class="h-100 w-100 img-thumbnail" alt="${title}"/ >
                                           </div>
-                                          <div class="card-body">
+                                          <div class="card-body col-8">
                                             <h2 class="card-title">${title}</h2>
                                             <p class="card-text">${body}</p>
                                             <p class="card-text">Date:${created}</p>
-                                            <a href="/post/edit/index.html?id=${id}" class="btn btn-primary">Edit Post</a> 
+                                            <a href="/post/edit/?id=${id}" class="btn btn-primary">Edit Post</a> 
                                             <a href="/post/specificPost/index.html?id=${id}" class= "btn btn-primary">View Post</a>
                                             <button data-delete-id=${id} class="delete-post">Delete Post</button>
                                           </div>
@@ -74,18 +56,19 @@ export function createProfilePostHtml(posts) {
 
 //**************Single post for specific page****** */
 
-// function createHTML(details) {
+// function createSinglePostHTML(post) {
 //   const specificPostContainer = document.getElementById("specificPost");
+//  specificPostContainer.innerHTML ="";
 
 //   specificPostContainer.innerHTML += ` <div class="card" style="width: 18rem;">
 //                                             <div class="">
-//                                                 <img src="${details.media}" class="card-img-top img-thumbnail style="width: 18rem;height : 18rem;" alt="${details.title}"/>
+//                                                 <img src="${post.media}" class="card-img-top img-thumbnail style="width: 18rem;height : 18rem;" alt="${details.title}"/>
 //                                             </div>
 //                                             <div class="card-body">
-//                                                 <h2 class="card-title">${details.title}</h2>
-//                                                 <p class="card-text">${details.body}</p>
-//                                                 <p class="card-text">By ${details.author}</p>
-//                                                 <p class="card-text">By ${details.created}</p>
+//                                                 <h2 class="card-title">${post.title}</h2>
+//                                                 <p class="card-text">${post.body}</p>
+//                                                 <p class="card-text">By ${post.author}</p>
+//                                                 <p class="card-text">By ${post.created}</p>
 
 //                                             </div>
 //                                         </div>`;
